695c071c5e9cedb3f13b469bf44a9540f7433b7b
DAIJIAYANG <3272103794@qq.com>
2026-04-16 13:43:29 +1000

    "Conflict in the modified version" minor error

--- src/app/privacy/privacy.module.ts
+++ src/app/privacy/privacy.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
 
 import { PrivacyPageRoutingModule } from './privacy-routing.module';
-
 import { PrivacyPage } from './privacy.page';
 
 @NgModule({
@@ -13,8 +11,9 @@ import { PrivacyPage } from './privacy.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    PrivacyPageRoutingModule
+    PrivacyPageRoutingModule,
+    PrivacyPage  
   ],
-  declarations: [PrivacyPage]
+  declarations: [] 
 })
 export class PrivacyPageModule {}
