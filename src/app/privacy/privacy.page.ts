d3ec34c13e8fa6cfcd099e76b2fe52c1e9c0eab8
DAIJIAYANG <3272103794@qq.com>
2026-04-16 13:47:17 +1000

    m "fix: imported IonicModule into standalone PrivacyPage"

--- src/app/privacy/privacy.page.ts
+++ src/app/privacy/privacy.page.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular'; // <-- Key point: Import the Ionic UI engine
 @Component({
   selector: 'app-privacy',
   templateUrl: './privacy.page.html',
   styleUrls: ['./privacy.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule] // <-- Key point: Inform the page about the use of these engines
 })
 export class PrivacyPage implements OnInit {
 
