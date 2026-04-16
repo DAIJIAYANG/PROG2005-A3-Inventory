6c2f3b53240be7ab002ec65236daf407066fbe13
DAIJIAYANG <3272103794@qq.com>
2026-04-16 11:49:49 +1000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PROG2005-A3-Inventory',
+  webDir: 'www'
+};
+
+export default config;
